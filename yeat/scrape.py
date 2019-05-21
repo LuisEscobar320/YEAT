4d6a47c4647b4f94f8586d7a97683198979e2994
James Riback <jriback144@student.msjc.edu>
2019-05-21 14:40:12 -0700

    Added updated scrape script for real this time

--- yeat/scrape.py
+++ yeat/scrape.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+
 from bs4 import BeautifulSoup
 import re
 import pandas as pd
@@ -8,9 +9,16 @@ import os
 from selenium import webdriver
 from selenium import webdriver
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
-
 from selenium.webdriver.support.select import Select
 from collections import defaultdict #for dict
+from firebase import firebase
+
+import json
+
+fb = firebase.FirebaseApplication('https://yeat-dc4bc.firebaseio.com/', None)
+
+#firebase.put('64Degrees','test',79)
+
 
 diningHallList = []
 #This script works for scraping one singular dining hall
@@ -37,15 +45,16 @@ class NutritionBoi:
 
 
 
-
 # create a new Firefox session
 driver = webdriver.Firefox(executable_path=r'C:\Users\jeem\Documents\CSE110\geckodriver.exe')
 
 # launch url
-url = "https://hdh-web.ucsd.edu/dining/apps/diningservices/Restaurants/MenuItem/64"
+url = "https://hdh-web.ucsd.edu/dining/apps/diningservices/Restaurants/MenuItem/05"
 binary = FirefoxBinary(r'C:\Program Files (x86)\Mozilla Firefox\firefox.exe')
+
 driver = webdriver.Firefox(firefox_binary=binary)
 driver.get(url)
+
 
 menus = [] #Breakfast, lunch, and dinner dictionaries
 
@@ -55,8 +64,6 @@ for i in range(3): #scrape for breakfast, lunch and dinner
     select_fr = Select(driver.find_element_by_id("mySelect"))
     select_fr.select_by_index(i)  # click breakfast, lunch and dinner
     driver.implicitly_wait(30)  # NEEDED SO WEBPAGE HAS TIME TO LOAD
-
-
 
     div = driver.find_element_by_id("containerMenuItems")
 
@@ -77,6 +84,7 @@ for i in range(3): #scrape for breakfast, lunch and dinner
         foodItemAndCost = li.find('a')
         foodItem = foodItemAndCost.contents[0]
         print(foodItemAndCost.contents[0])
+
         #print(foodItemAndCost)
         #nb = NutritionBoi() #Create a nutrition object to associate with each food item
         for img in li.find_all('img'):
@@ -93,7 +101,33 @@ for i in range(3): #scrape for breakfast, lunch and dinner
                    # nb.setDairy(True) #if it is set the nutrition boi to true
                 #etc
                 #need to add OR the react side can parse the nutritional legend and i just send as is
+
     menus.append(foodDict)
 
 for i in menus:
     print(i)
+#firebase.put(f'/64Degrees-BREAKFAST', '$', 'MONEY!')
+
+times = ["Breakfast", "Lunch", "Dinner"]
+print("Pushing to firebase...")
+for i in range(3):
+    for k, v in menus[i].items():
+        newKey = "".join("".join(k.split())) #remove white space
+        #newKey =  newKey.replace("$", " ") #remove dollar sign
+        #newKey = json.dumps(newKey)
+        head, sep, tail = newKey.partition('$')#cost of food item if exists
+        print(head)
+        print(tail)
+
+        print("key " + head)
+        v = ' '.join(v) #convert list to string
+        newNutritionList = {v.replace('Legend:', '').replace('Icon', ',').replace('Contains', '') for x in v} #remove unnecessary icon stuff
+        print("contains")
+        newNutritionList = list(newNutritionList)
+        #nutrition_json = json.dumps(newNutritionList)
+
+
+        print(newNutritionList)
+
+        fb.put(f'/CafeVentas-{times[i]}', head, {'cost': sep + tail, 'Nutrition' : newNutritionList})
+        #fb.post(f'/64Degrees-{times[i]}' , {head: nutrition_json } )
